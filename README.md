d7c4672eb27915a6f78de9e874548c40b30475bc
Antônio Neto <soh.tecnologia@gmail.com>
2024-01-18 14:18:44 -0300

    implementation using a head and a tail

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## LRU Cache Example (WORK IN PROGRESS)
 A toy project to learn the basics of LRU caching
+
+https://www.geeksforgeeks.org/lru-cache-implementation/
+
--- main.go
+++ main.go
@@ -2,43 +2,107 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
+var logr = log.New(os.Stderr, "ERROR:", 2)
+
 type LRUCache struct {
-	Capacity   int
-	CachedData map[string]*CachedItem
+	head       *CachedPair //leftmost item (last accessed item)
+	tail       *CachedPair //rightmost item (least accesed item)
+	capacity   int
+	cachedData map[string]*CachedPair
 }
 
-type CachedItem struct {
+type CachedPair struct {
 	Key      string
 	Value    any
-	previous *CachedItem
-	next     *CachedItem
+	previous *CachedPair
+	next     *CachedPair
 }
 
 func NewLRUCache(capacity int) *LRUCache {
 	return &LRUCache{
-		Capacity:   capacity,
-		CachedData: make(map[string]*CachedItem),
+		capacity:   capacity,
+		cachedData: make(map[string]*CachedPair),
 	}
 }
 
-func (c *LRUCache) Get(key string) *CachedItem {
-	if _, ok := c.CachedData[key]; !ok {
+func (c *LRUCache) Get(key string) (any, error) {
+	if c.head == nil {
+		return nil, fmt.Errorf(("can't retrieve data from empty cache"))
+	}
+
+	cachedPair, ok := c.cachedData[key]
+	if !ok {
+		return nil, fmt.Errorf("no value stored for the given key")
+	}
+
+	// last acessed item must be at the leftmost position (head)
+	c.setNewHead(c.cachedData[key])
+
+	//if a cached item exists for the given key, it's value is not nil
+	return cachedPair.Value, nil
+}
+
+func (c *LRUCache) setNewHead(newHead *CachedPair) {
+	if c.head.next == nil {
+		return
+	}
+
+	newHeadOldPrevious := newHead.previous
+	newHeadOldNext := newHead.previous
+
+	newHeadOldPrevious.next = newHeadOldNext
+
+	newHeadOldNext.previous = newHeadOldPrevious
+
+	newHead.next = c.head
+	newHead.previous = nil
+	c.head = newHead
+
+}
+
+func (c *LRUCache) Insert(key string, value any) error {
+	if c == nil {
+		return fmt.Errorf("non-initialized cache")
+	}
+
+	if value == nil {
+		return fmt.Errorf("cannot insert a nil value")
+	}
+
+	if c.head == nil {
+		c.head = &CachedPair{Key: key, Value: value, previous: nil, next: nil}
+		c.tail = c.head
+		c.cachedData[key] = c.head
 		return nil
 	}
 
-	return c.CachedData[key]
-}
+	if len(c.cachedData) == c.capacity {
+		delKey := c.tail.Key
+		c.tail = c.tail.previous
+		c.tail.next = nil
+		delete(c.cachedData, delKey)
+	}
 
-func (c *LRUCache) Set(key string) any {
-	return c.insert(key)
-}
+	oldTail := c.tail
 
-func (c *LRUCache) insert(key string) any {
-	return -1
+	c.tail = &CachedPair{Key: key, Value: value}
+	c.tail.previous = oldTail
+	c.tail.next = nil
+
+	oldTail.next = c.tail
+
+	return nil
 }
 
 func main() {
-	fmt.Println("LRU Caching")
+
+	cache := NewLRUCache(3)
+
+	if err := cache.Insert("oi", nil); err != nil {
+		logr.Printf("%v", err)
+	}
 }
